2525720773495529e6e26c7aa9bf4b0ab4dc2f65
Sudhanshu-Ambastha <sudhanshuambastha2020@gmail.com>
2025-03-16 17:32:46 +0530

    ⬆update

--- server/routes/db.route.js
+++ server/routes/db.route.js
@@ -2,205 +2,97 @@ import express from 'express';
 const router = express.Router();
 
 import {
-  RegularPokemon,
-  ShinyPokemon,
-  AlolanPokemon,
-  HisuianPokemon,
-  MegaPokemon,
-  GalarPokemon,
-  GmaxPokemon,
-  MegaShinyPokemon,
-  MultiFormPokemon,
-  ShadowPokemon,
-  UniquePokemon,
-  XyPokemon,
-  PrimalPokemon,
-  OriginPokemon,
+    RegularPokemon,
+    ShinyPokemon,
+    AlolanPokemon,
+    HisuianPokemon,
+    MegaPokemon,
+    GalarPokemon,
+    GmaxPokemon,
+    MegaShinyPokemon,
+    MultiFormPokemon,
+    ShadowPokemon,
+    UniquePokemon,
+    XyPokemon,
+    PrimalPokemon,
+    OriginPokemon,
 } from '../models/db.model.js';
 
 router.get('/ping', (req, res) => {
-  res.status(200).send('pong');
+    res.status(200).send('pong');
 });
 
-router.get('/regular', async (req, res) => {
-  try {
-    const pokemonData = await RegularPokemon.find({}).limit(721);
-    res.json(pokemonData);
-  } catch (error) {
-    console.error('Error fetching regular Pokemon:', error);
-    res.status(500).json({ error: 'Failed to fetch regular Pokemon' });
-  }
-});
+// Function to apply limit and offset
+const applyLimitAndOffset = async (Model, req, res) => {
+    const limit = parseInt(req.query.limit) || 10;
+    const offset = parseInt(req.query.offset) || 0;
 
-router.get('/shiny', async (req, res) => {
-  try {
-    const pokemonData = await ShinyPokemon.find({});
-    res.json(pokemonData);
-  } catch (error) {
-    console.error('Error fetching shiny Pokemon:', error);
-    res.status(500).json({ error: 'Failed to fetch shiny Pokemon' });
-  }
-});
+    try {
+        const pokemonData = await Model.find({}).skip(offset).limit(limit);
+        res.json(pokemonData);
+    } catch (error) {
+        console.error(`Error fetching ${Model.modelName} Pokemon:`, error);
+        res.status(500).json({ error: `Failed to fetch ${Model.modelName} Pokemon` });
+    }
+};
 
-router.get('/alolan', async (req, res) => {
-  try {
-    const pokemonData = await AlolanPokemon.find({});
-    res.json(pokemonData);
-  } catch (error) {
-    console.error('Error fetching Alolan Pokemon:', error);
-    res.status(500).json({ error: 'Failed to fetch Alolan Pokemon' });
-  }
-});
-
-router.get('/hisuian', async (req, res) => {
-  try {
-    const pokemonData = await HisuianPokemon.find({});
-    res.json(pokemonData);
-  } catch (error) {
-    console.error('Error fetching Hisuian Pokemon:', error);
-    res.status(500).json({ error: 'Failed to fetch Hisuian Pokemon' });
-  }
-});
-
-router.get('/mega', async (req, res) => {
-  try {
-    const pokemonData = await MegaPokemon.find({});
-    res.json(pokemonData);
-  } catch (error) {
-    console.error('Error fetching Mega Pokemon:', error);
-    res.status(500).json({ error: 'Failed to fetch Mega Pokemon' });
-  }
-});
-
-router.get('/galar', async (req, res) => {
-  try {
-    const pokemonData = await GalarPokemon.find({});
-    res.json(pokemonData);
-  } catch (error) {
-    console.error('Error fetching Galar Pokemon:', error);
-    res.status(500).json({ error: 'Failed to fetch Galar Pokemon' });
-  }
-});
-
-router.get('/gmax', async (req, res) => {
-  try {
-    const pokemonData = await GmaxPokemon.find({});
-    res.json(pokemonData);
-  } catch (error) {
-    console.error('Error fetching Gmax Pokemon:', error);
-    res.status(500).json({ error: 'Failed to fetch Gmax Pokemon' });
-  }
-});
-
-router.get('/mega-shiny', async (req, res) => {
-  try {
-    const pokemonData = await MegaShinyPokemon.find({});
-    res.json(pokemonData);
-  } catch (error) {
-    console.error('Error fetching Mega Shiny Pokemon:', error);
-    res.status(500).json({ error: 'Failed to fetch Mega Shiny Pokemon' });
-  }
-});
-
-router.get('/multi-form', async (req, res) => {
-  try {
-    const pokemonData = await MultiFormPokemon.find({});
-    res.json(pokemonData);
-  } catch (error) {
-    console.error('Error fetching Multi Form Pokemon:', error);
-    res.status(500).json({ error: 'Failed to fetch Multi Form Pokemon' });
-  }
-});
-
-router.get('/shadow', async (req, res) => {
-  try {
-    const pokemonData = await ShadowPokemon.find({});
-    res.json(pokemonData);
-  } catch (error) {
-    console.error('Error fetching Shadow Pokemon:', error);
-    res.status(500).json({ error: 'Failed to fetch Shadow Pokemon' });
-  }
-});
-
-router.get('/unique', async (req, res) => {
-  try {
-    const pokemonData = await UniquePokemon.find({});
-    res.json(pokemonData);
-  } catch (error) {
-    console.error('Error fetching Unique Pokemon:', error);
-    res.status(500).json({ error: 'Failed to fetch Unique Pokemon' });
-  }
-});
-
-router.get('/xy', async (req, res) => {
-  try {
-    const pokemonData = await XyPokemon.find({});
-    res.json(pokemonData);
-  } catch (error) {
-    console.error('Error fetching XY Pokemon:', error);
-    res.status(500).json({ error: 'Failed to fetch XY Pokemon' });
-  }
-});
-
-router.get('/primal', async (req, res) => {
-  try {
-    const pokemonData = await PrimalPokemon.find({});
-    res.json(pokemonData);
-  } catch (error) {
-    console.error('Error fetching Primal Pokemon:', error);
-    res.status(500).json({ error: 'Failed to fetch Primal Pokemon' });
-  }
-});
-
-router.get('/origin', async (req, res) => {
-  try {
-    const pokemonData = await OriginPokemon.find({});
-    res.json(pokemonData);
-  } catch (error) {
-    console.error('Error fetching Origin Pokemon:', error);
-    res.status(500).json({ error: 'Failed to fetch Origin Pokemon' });
-  }
-});
+router.get('/regular', (req, res) => applyLimitAndOffset(RegularPokemon, req, res));
+router.get('/shiny', (req, res) => applyLimitAndOffset(ShinyPokemon, req, res));
+router.get('/alolan', (req, res) => applyLimitAndOffset(AlolanPokemon, req, res));
+router.get('/hisuian', (req, res) => applyLimitAndOffset(HisuianPokemon, req, res));
+router.get('/mega', (req, res) => applyLimitAndOffset(MegaPokemon, req, res));
+router.get('/galar', (req, res) => applyLimitAndOffset(GalarPokemon, req, res));
+router.get('/gmax', (req, res) => applyLimitAndOffset(GmaxPokemon, req, res));
+router.get('/mega-shiny', (req, res) => applyLimitAndOffset(MegaShinyPokemon, req, res));
+router.get('/multi-form', (req, res) => applyLimitAndOffset(MultiFormPokemon, req, res));
+router.get('/shadow', (req, res) => applyLimitAndOffset(ShadowPokemon, req, res));
+router.get('/unique', (req, res) => applyLimitAndOffset(UniquePokemon, req, res));
+router.get('/xy', (req, res) => applyLimitAndOffset(XyPokemon, req, res));
+router.get('/primal', (req, res) => applyLimitAndOffset(PrimalPokemon, req, res));
+router.get('/origin', (req, res) => applyLimitAndOffset(OriginPokemon, req, res));
 
 router.get('/all', async (req, res) => {
-  try {
-    const regular = await RegularPokemon.find({}).limit(721);
-    const shiny = await ShinyPokemon.find({});
-    const alolan = await AlolanPokemon.find({});
-    const hisuian = await HisuianPokemon.find({});
-    const mega = await MegaPokemon.find({});
-    const galar = await GalarPokemon.find({});
-    const gmax = await GmaxPokemon.find({});
-    const megaShiny = await MegaShinyPokemon.find({});
-    const multiForm = await MultiFormPokemon.find({});
-    const shadow = await ShadowPokemon.find({});
-    const unique = await UniquePokemon.find({});
-    const xy = await XyPokemon.find({});
-    const primal = await PrimalPokemon.find({});
-    const origin = await OriginPokemon.find({});
+    const limit = parseInt(req.query.limit) || 10;
+    const offset = parseInt(req.query.offset) || 0;
 
-    const allPokemon = [
-      ...regular,
-      ...shiny,
-      ...alolan,
-      ...hisuian,
-      ...mega,
-      ...galar,
-      ...gmax,
-      ...megaShiny,
-      ...multiForm,
-      ...shadow,
-      ...unique,
-      ...xy,
-      ...primal,
-      ...origin,
-    ];
+    try {
+        const regular = await RegularPokemon.find({}).limit(721).skip(offset).limit(limit);
+        const shiny = await ShinyPokemon.find({}).skip(offset).limit(limit);
+        const alolan = await AlolanPokemon.find({}).skip(offset).limit(limit);
+        const hisuian = await HisuianPokemon.find({}).skip(offset).limit(limit);
+        const mega = await MegaPokemon.find({}).skip(offset).limit(limit);
+        const galar = await GalarPokemon.find({}).skip(offset).limit(limit);
+        const gmax = await GmaxPokemon.find({}).skip(offset).limit(limit);
+        const megaShiny = await MegaShinyPokemon.find({}).skip(offset).limit(limit);
+        const multiForm = await MultiFormPokemon.find({}).skip(offset).limit(limit);
+        const shadow = await ShadowPokemon.find({}).skip(offset).limit(limit);
+        const unique = await UniquePokemon.find({}).skip(offset).limit(limit);
+        const xy = await XyPokemon.find({}).skip(offset).limit(limit);
+        const primal = await PrimalPokemon.find({}).skip(offset).limit(limit);
+        const origin = await OriginPokemon.find({}).skip(offset).limit(limit);
 
-    res.json(allPokemon); 
-  } catch (error) {
-    console.error('Error fetching all Pokémon:', error);
-    res.status(500).json({ error: 'Failed to fetch all Pokémon' });
-  }
+        const allPokemon = [
+            ...regular,
+            ...shiny,
+            ...alolan,
+            ...hisuian,
+            ...mega,
+            ...galar,
+            ...gmax,
+            ...megaShiny,
+            ...multiForm,
+            ...shadow,
+            ...unique,
+            ...xy,
+            ...primal,
+            ...origin,
+        ];
+
+        res.json(allPokemon);
+    } catch (error) {
+        console.error('Error fetching all Pokémon:', error);
+        res.status(500).json({ error: 'Failed to fetch all Pokémon' });
+    }
 });
-    
+
 export default router;
