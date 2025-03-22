94795377ab58a9e3312d7ed0ef373d8a29f289f3
Sudhanshu-Ambastha <sudhanshuambastha2020@gmail.com>
2025-03-22 07:06:12 +0530

    🛠fix

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -5,6 +5,7 @@ DEST_DIR="models/gltfjsx"
 
 echo "🔄 Checking GLB files for conversion..."
 modified=false
+all_present=true
 
 find "$SRC_DIR" -type f -name "*.glb" | while read -r glb_file; do
   # Extract folder and filename
@@ -13,7 +14,7 @@ find "$SRC_DIR" -type f -name "*.glb" | while read -r glb_file; do
   modelname="${filename%.glb}"
   jsx_file="$DEST_DIR/$folder/$modelname.jsx"
 
-  # Check if JSX file exists and is up to date (with buffer)
+  # Check if JSX file exists and is up to date
   if [[ -f "$jsx_file" && $(stat -c %Y "$jsx_file") -gt $(( $(stat -c %Y "$glb_file") + 1 )) ]]; then
     continue # Skip if JSX file is newer than GLB + 1 second
   fi
@@ -24,12 +25,13 @@ find "$SRC_DIR" -type f -name "*.glb" | while read -r glb_file; do
   # Process the GLB file
   npx gltfjsx@6.5.3 "$glb_file" -o "$jsx_file"
   modified=true
+  all_present=false
   echo "🔄 Converted: $glb_file to $jsx_file" # Informative Message.
 done
 
 # Print the appropriate message
 if [ "$modified" = true ]; then
   echo "✅ Conversion completed!"
-else
+elif [ "$all_present" = true ]; then
   echo "✨ All already present!"
 fi
